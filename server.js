c9c66a704d29d016a6b2c3bdb0e9f915a461c1ab
saikrishnanarravula <79821265+saikrishnanarravula@users.noreply.github.com>
2022-04-12 14:17:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, Hope you are doing well & Welcome to the POC GCP !!');
+  res.send('Hello World, Hope you are doing well & Welcome to the POC GCP one !!');
 });
 
 app.listen(PORT, HOST);
